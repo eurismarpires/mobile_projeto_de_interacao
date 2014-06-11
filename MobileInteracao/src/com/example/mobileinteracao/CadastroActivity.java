635617bfd83eb5c306b19e1754b8e6eeed2e8c63
Eurismar Pires Borges <eurismarpires@gmail.com>
2014-06-10 23:55:34 -0300

    criado a activity de detalhe da mensage e retirado os fragments

--- MobileInteracao/src/com/example/mobileinteracao/CadastroActivity.java
+++ MobileInteracao/src/com/example/mobileinteracao/CadastroActivity.java
@@ -3,7 +3,9 @@ package com.example.mobileinteracao;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,47 +20,12 @@ public class CadastroActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_cadastro);
 
-		if (savedInstanceState == null) {
-			getFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.cadastro, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_cadastro,
-					container, false);
-			return rootView;
-		}
+	public void onClick(View v){
+		Log.i("Teste", "C . L . I . C . O . U");
+		Intent intent = new Intent(this, MainActivity.class);
+		startActivity(intent);
 	}
 
 }
--- MobileInteracao/src/com/example/mobileinteracao/DetalheMensagemActivity.java
+++ MobileInteracao/src/com/example/mobileinteracao/DetalheMensagemActivity.java
@@ -3,7 +3,6 @@ package com.example.mobileinteracao;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -12,17 +11,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 
-public class MainActivity extends Activity {
+public class DetalheMensagemActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
-
-	public void onClick(View v){
-		Intent intent = new Intent(this, CadastroActivity.class);
-		startActivity(intent);
+		setContentView(R.layout.activity_detalhe_mensagem);
 	}
 
 
